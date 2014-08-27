77888a6fbe4cacc578db54cf81dd33b98c6a216f
Chris Lee <chris@cglee.me>
2014-08-27 18:20:04 -0500

    computer defends two in a row

--- ttt.rb
+++ ttt.rb
@@ -1,5 +1,5 @@
 # Initialize board hash
-# Draw board 
+# Draw board
 # Choose at random who goes first
 
 # Player/Computer has a turn if square available
@@ -16,7 +16,7 @@ while play_again == 'y'
 board_squares = {1 => " ",2 => " ",3 => " ",4 => " ",5 => " ",6 => " ",7 => " ",8 => " ",9 => " "}
 
 # rows columns and diagonals
-lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+WINNING_LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 
 # Available squares
 def available_squares(squares)
@@ -26,7 +26,7 @@ end
 # players stored in array so player can be chosen at random
 players = ["player1", "player2"]
 
-# draw the board 
+# draw the board
 def draw_board(squares)
   system 'clear'
   puts "#{squares[1]}|#{squares[2]}|#{squares[3]}"
@@ -49,15 +49,15 @@ end
 
 
 # player/computer picks square methods
-def player1(squares) 
+def player1(squares)
     if available_squares(squares).any?
       puts "Choose an available square from #{available_squares(squares)}"
-      i = gets.chomp.to_i    
+      i = gets.chomp.to_i
         if available_squares(squares).include?(i)
           squares[i] = "x"
         else
           player1(squares)
-        end     
+        end
       draw_board(squares)
     end
 end
@@ -66,14 +66,29 @@ def defend(squares)
 
 end
 
+def two_in_a_row?(hsh)
+  if hsh.values.count('x') == 2
+    return hsh.select{|k,v| v != 'x'}.keys.first
+  else
+    return false
+  end
+end
+
 def player2(line, squares)
     puts "Computer chooses a square"
     sleep 0.5
 
 # if player has two positions in a line and the other is empty defend else attack < not yet implemented
+    defend_this_square = nil
+    WINNING_LINES.each do |l|
+      defend_this_square = two_in_a_row?({l[0] => squares[l[0]], l[1] => squares[l[1]], l[2] => squares[l[2]]})
+      if defend_this_square
+        squares[defend_this_square] = 'o'
+        break
+      end
+    end
 
-    i = available_squares(squares).sample
-    squares[i] = "o"
+    squares[available_squares(squares).sample] = "o" unless defend_this_square
     draw_board(squares)
 end
 
@@ -86,16 +101,16 @@ draw_board(board_squares)
 # Conditional that checks which loop to execute: player 1 or player 2
 if goes_first == 'player1'
   begin
-    break if check_for_winner(lines, board_squares) == true
+    break if check_for_winner(WINNING_LINES, board_squares) == true
     player1(board_squares)
-    break if check_for_winner(lines, board_squares) == true
-    player2(lines, board_squares)
+    break if check_for_winner(WINNING_LINES, board_squares) == true
+    player2(WINNING_LINES, board_squares)
   end until available_squares(board_squares).empty?
 elsif goes_first == 'player2'
   begin
-    break if check_for_winner(lines, board_squares) == true
-    player2(lines, board_squares)
-    break if check_for_winner(lines, board_squares) == true
+    break if check_for_winner(WINNING_LINES, board_squares) == true
+    player2(WINNING_LINES, board_squares)
+    break if check_for_winner(WINNING_LINES, board_squares) == true
     player1(board_squares)
   end until available_squares(board_squares).empty?
 end
@@ -116,6 +131,6 @@ end
 
 
 
-   
+
 
 
